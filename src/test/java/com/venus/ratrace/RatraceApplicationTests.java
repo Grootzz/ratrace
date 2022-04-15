4a5423c5ed8a354151af4f4458bcc4b5b3076715
anxu <noodle.ax@gmail.com>
2022-04-15 21:02:21 +0800

    init repo

---
+++ src/test/java/com/venus/ratrace/RatraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venus.ratrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
